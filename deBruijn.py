ce88ba3dab374e7484d6a83eb344211e39d6548d
Essux <jjsuarestra99@gmail.com>
2017-04-10 15:23:26 -0500

    Compatibility with multiple reads and GraphViz export

--- deBruijn.py
+++ deBruijn.py
@@ -1,16 +1,30 @@
-def de_bruijn_ize(st, k):
-    edges = {}
-    nodes = set()
-    for i in range(len(st) - k +1):
-        kmer1 = st[i:i+k-1]
-        kmer2 = st[i+1:i+k]
-        if kmer1 not in edges.keys():
-            edges[kmer1] = []
-        edges[kmer1].append(kmer2)
+edges = {}
+nodes = set()
+kmers = set()
 
-        nodes.add(st[i:i+k-1])
-        nodes.add(st[i+1:i+k])
-    edges['INITIAL'] = [st[:k-1]]
+def de_bruijn_ize(st, k):
+    if st[:k] not in kmers and st[:k-1] not in edges:
+        if 'INITIAL' not in edges:
+            edges['INITIAL'] = []
+        edges['INITIAL'].append(st[:k-1])
+
+    for i in range(len(st) - k +1):
+        kmer = st[i:i+k]
+        if kmer not in kmers:
+            kmers.add(kmer)
+            kmer1 = st[i:i+k-1]
+            kmer2 = st[i+1:i+k]
+            if kmer1 not in edges.keys():
+                edges[kmer1] = []
+            edges[kmer1].append(kmer2)
+            
+            #If an edge goes towards it, it's not really an initial
+            if kmer2 in edges['INITIAL']:
+                index = edges['INITIAL'].index(kmer2)
+                del edges['INITIAL'][index]
+
+            nodes.add(st[i:i+k-1])
+            nodes.add(st[i+1:i+k])
     for node in nodes:
         if node not in edges:
             edges[node] = []
@@ -19,18 +33,18 @@ def de_bruijn_ize(st, k):
 visited = set()
 trail = []
 
-def dfs(curNode, edges, initial):
+def dfs(curNode, initial):
     visited.add(curNode)
     while curNode in edges and edges[curNode]:
         nextNode = edges[curNode].pop()
-        dfs(nextNode, edges, False)
-    if (not initial):
+        dfs(nextNode, False)
+    if not initial:
         trail.append(curNode)
 
-def topoSort(nodes, edges):
-    for node in edges.keys():
-        if edges[node]:
-            dfs(node, edges, True)
+def topoSort(nodes):
+    for initialnode in edges['INITIAL']:
+        if edges[initialnode] and initialnode not in visited:
+            dfs(initialnode, False)
     trail.reverse()
 
 def listToString(fragments):
@@ -39,14 +53,39 @@ def listToString(fragments):
         st += fragments.pop(0)[-1]
     return st
 
-#st = 'ACTAGAGTTTTTTTGAT'
-st = input('/')
-nodes, edges = de_bruijn_ize(st, 7)
-print(nodes)
-print(edges)
-topoSort(nodes, edges)
-print(trail)
-print(st)
+def graphvizExport():
+        text = ""
+        text += "digraph deBruijn {\n"
+        text += "node [shape = circle];\n"
+        for f in edges2.keys():
+            for t in edges2[f]:
+                text += str(f) + " -> " + str(t) + '\n'
+        text += "}"
+
+        with open("deBruijn.dot", encoding='utf-8', mode='w') as file:
+            file.write(text)
+
+final = input('/')
+k = int(input('Type k (for the kmers):'))
+reads = int(input('Type the number of reads to be simulated:'))
+import random
+for i in range(0, reads):
+    try:
+        rand1= random.randint(0, len(final) - k)
+        rand2 = random.randint(rand1+k, len(final))
+        de_bruijn_ize(final[rand1:rand2], k)
+        print('Read', i+1, final[rand1:rand2])
+    except:
+        print('Failed Read')
+import copy
+edges2 = copy.deepcopy(edges)
+#print('Nodes:', nodes)
+print('Edges:', edges)
+#print('Kmers', kmers)
+topoSort(nodes)
+print('Trail:', trail)
+print(final)
 built = listToString(trail)
 print(built)
-print('Equal:', st == built)
+print('Equal:', final == built)
+graphvizExport()
